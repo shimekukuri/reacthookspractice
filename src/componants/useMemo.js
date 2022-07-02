00513ff94749aece7581c8b5846d3fd9bc63d430
shimekukuri <jhutchinson192390@gmail.com>
2022-07-02 11:29:07 -0400

    initializedd react application to test different hooks

---
+++ src/componants/useMemo.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useMemo} from "react";
+
+export default function MemoPractice() {
+    const [number, setNumber] = useState()
+    const [theme, setTheme] = useState(false)
+
+    return(<></>)
+}
