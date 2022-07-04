606f6830d19eaa299b79035b4a3fd526649a0006
shimekukuri <jhutchinson192390@gmail.com>
2022-07-03 20:17:18 -0400

    added a placeholder to useReducerPractice input box

--- src/componants/useReducer.js
+++ src/componants/useReducer.js
@@ -49,6 +49,7 @@ export default function UseReducerPractice() {
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            placeholder="Enter a todo"
           />
         </form>
         {todos.map((todo) => (
